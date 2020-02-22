7114721060e6e1ecff0508b57d8156a16cf47d56
Luis Pineda <lspnd00@gmail.com>
2020-02-21 20:41:55 -0500

    finished 3 sections MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container')
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    const articles1 = Object.values(response.data.articles)
+    // console.log(articles1)
+    articles1.forEach(item => {
+      console.log(item)
+      item.forEach(item2 => {
+        cardContainer.appendChild(cardComp(item2))
+      })
+    })
+    // ;[response.data.articles].forEach(article => {
+    //   console.log(article)
+    //   const newCardComp = cardComp(article)
+    //   cardContainer.appendChild(newCardComp)
+    // })
+    //   const newCardComp = cardComp(article)
+    //   cardContainer.appendChild(newCardComp)
+  })
+
+function cardComp(article) {
+  const topDiv = document.createElement('div')
+  topDiv.classList.add('card')
+
+  const headlineDiv = document.createElement('div')
+  headlineDiv.classList.add('headline')
+  headlineDiv.textContent = article.headline
+  topDiv.appendChild(headlineDiv)
+
+  const authorDiv = document.createElement('div')
+  authorDiv.classList.add('author')
+  topDiv.appendChild(authorDiv)
+
+  const imgContainer = document.createElement('div')
+  imgContainer.classList.add('img-container')
+  authorDiv.appendChild(imgContainer)
+
+  const image = document.createElement('img')
+  image.src = article.authorPhoto
+  imgContainer.appendChild(image)
+
+  const authorName = document.createElement('span')
+  authorName.textContent = article.authorName
+  // might need to add class idk yet
+  authorDiv.appendChild(authorName)
+
+  return topDiv
+}
